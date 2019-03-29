fb8b4e72a987b3c7810faedd20ac8da667aec1f1
Boris Skert <boris.skert@gmail.com>
2019-03-29 12:41:20 +0100

    Fix unit tests

--- angular/src/app/store/services/store.service.spec.ts
+++ angular/src/app/store/services/store.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { StoreService } from './store.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideMockedAppConfig } from '../../app.config.mock';
 
 describe('StoreService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule ],
+    providers: [
+      provideMockedAppConfig()
+    ]
+  }));
 
   it('should be created', () => {
     const service: StoreService = TestBed.get(StoreService);
