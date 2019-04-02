b651692feeea59e0266fb6a35c22e2c3159b80b1
Boris Skert <boris.skert@gmail.com>
2019-04-02 09:32:43 +0200

    [issue/#14] Adjust credentials for travis-ci.com (#15)

--- .travis/deploy.bash
+++ .travis/deploy.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
+
 .travis/deploy_springboot.bash
 .travis/deploy_angular.bash
--- .travis/deploy_springboot.bash
+++ .travis/deploy_springboot.bash
@@ -4,5 +4,4 @@ IMAGE_NAME=borisskert/cats-n-dogs:springboot
 
 docker build -t "${IMAGE_NAME}" ./springboot
 
-docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 docker push "${IMAGE_NAME}"
